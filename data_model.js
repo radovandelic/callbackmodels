e6e4b7a1da0ff00dd92de056c1aeec2441717924
Radovan Delic <infamouszgbgd@yahoo.com>
2017-09-26 15:38:28 +0200

    initial commit

---
+++ data_model.js
@@ -0,0 +1,33 @@
+var data_model_array = {
+    count: 0,
+    db: [], // array of objects
+    create: function (new_object) {
+        var id = this.count;
+        this.count++;
+        this.db[id] = new_object;
+        // assigns a unique ID to each object
+        //   once an ID has been used for one object, it can never be used for another
+        //   once an object has been given an ID, it can never be given a new one
+        // adds the object to db
+    },
+    read: function (entry_id) {
+        return this.db[entry_id];
+        // object associated with that id
+    },
+    update: function (entry_id, new_value) {
+        this.db[entry_id] = new_value;
+        // replace said entry with said value
+    },
+    remove: function (entry_id) {
+        this.db.splice(entry_id, 1);
+        // remove object with said id
+        // this operation must not change other objects' id's
+    }
+};
+
+var db = [];
+var data_model_object = {
+    db: {}
+    // object of objects
+	// ...
+};

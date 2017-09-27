562a3b3fc20d2d543f3760362bc9287b8dea5781
Radovan Delic <infamouszgbgd@yahoo.com>
2017-09-27 14:33:58 +0200

    returns

--- data_model.js
+++ data_model.js
@@ -5,34 +5,39 @@ var data_model_array = {
         var id = this.count;
         this.count++;
         this.db[id] = new_object;
+        return this.db[id];
     },
     read: function (entry_id) {
         return this.db[entry_id];
     },
     update: function (entry_id, new_value) {
         this.db[entry_id] = new_value;
+        return this.db[entry_id];
     },
     remove: function (entry_id) {
         this.db.splice(entry_id, 1);
+        return "Object #" + entry_id + " removed.";
     }
 };
 
-var db = [];
 var data_model_object = {
     count: 0,
-    db: {}, // object of objectss
+    db: {}, // object of objects
     create: function (new_object) {
         var id = this.count;
         this.count++;
         this.db[id] = new_object;
+        return this.db[id];
     },
     read: function (entry_id) {
         return this.db[entry_id];
     },
     update: function (entry_id, new_value) {
         this.db[entry_id] = new_value;
+        return this.db[entry_id];
     },
     remove: function (entry_id) {
         delete this.db[entry_id];
+        return "Object #" + entry_id + " removed.";
     }
 };
